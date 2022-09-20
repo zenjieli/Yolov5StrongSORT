f0d45984bd391d9cb97e022048ec6cd43804a46c
Zenjie Li <zli@milestone.dk>
2022-09-20 15:38:47 +0200

    Sequential processes in val.py

--- val.py
+++ val.py
@@ -30,7 +30,7 @@ from track import run
 
 
 def setup_evaluation(dst_val_tools_folder):
-    
+
     # source: https://github.com/JonathonLuiten/TrackEval#official-evaluation-code
     LOGGER.info('Download official MOT evaluation repo')
     val_tools_url = "https://github.com/JonathonLuiten/TrackEval"
@@ -38,7 +38,7 @@ def setup_evaluation(dst_val_tools_folder):
         Repo.clone_from(val_tools_url, dst_val_tools_folder)
     except git.exc.GitError as err:
         LOGGER.info('Eval repo already downloaded')
-        
+
     LOGGER.info('Get ground-truth txts, meta-data and example trackers for all currently supported benchmarks')
     gt_data_url = 'https://omnomnom.vision.rwth-aachen.de/data/TrackEval/data.zip'
     subprocess.run(["wget", "-nc", gt_data_url, "-O", dst_val_tools_folder / 'data.zip']) # python module has no -nc nor -N flag
@@ -52,8 +52,8 @@ def setup_evaluation(dst_val_tools_folder):
     if not (dst_val_tools_folder / 'data' / 'MOT16').is_dir():
         with zipfile.ZipFile(dst_val_tools_folder / 'MOT16.zip', 'r') as zip_ref:
             zip_ref.extractall(dst_val_tools_folder / 'data' / 'MOT16')
-        
-    
+
+
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--yolo-weights', nargs='+', type=str, default=WEIGHTS / 'crowdhuman_yolov5m.pt', help='model.pt path(s)')
@@ -73,11 +73,11 @@ def parse_opt():
 
 def main(opt):
     check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
-    
+
     # download eval files
     dst_val_tools_folder = ROOT / 'val_utils'
     setup_evaluation(dst_val_tools_folder)
-    
+
     # set paths
     mot_seqs_path = dst_val_tools_folder / 'data' / opt.benchmark / opt.split
     seq_paths = [p / 'img1' for p in Path(mot_seqs_path).iterdir() if p.is_dir()]
@@ -85,12 +85,12 @@ def main(opt):
     MOT_results_folder = dst_val_tools_folder / 'data' / 'trackers' / 'mot_challenge' / Path(str(opt.benchmark) + '-' + str(opt.split)) / save_dir.name / 'data'
     (MOT_results_folder).mkdir(parents=True, exist_ok=True)  # make
 
-    
+
     if not opt.eval_existing:
 
         processes = []
         nr_gpus = torch.cuda.device_count()
-        
+
         for i, seq_path in enumerate(seq_paths):
 
             device = i % nr_gpus
@@ -104,7 +104,7 @@ def main(opt):
                 "python", "track.py",\
                 "--yolo-weights", "weights/crowdhuman_yolov5m.pt",\
                 "--strong-sort-weights",  "weights/osnet_x1_0_dukemtmcreid.pt",\
-                "--imgsz", str(1280),\
+                "--imgsz", str(800),\
                 "--classes", str(0),\
                 "--name", save_dir.name,\
                 "--project", opt.project,\
@@ -114,16 +114,17 @@ def main(opt):
                 "--save-txt",\
                 "--eval"
             ])
-            processes.append(p)
-        
-        for p in processes:
             p.wait()
+            # processes.append(p)
+
+        # for p in processes:
+        #     p.wait()
 
     results = (save_dir.parent / opt.eval_existing / 'tracks' if opt.eval_existing else save_dir / 'tracks').glob('*.txt')
     for src in results:
         if opt.eval_existing:
             dst = MOT_results_folder.parent.parent / opt.eval_existing / 'data' / Path(src.stem + '.txt')
-        else:  
+        else:
             dst = MOT_results_folder / Path(src.stem + '.txt')
         dst.parent.mkdir(parents=True, exist_ok=True)  # make
         shutil.copyfile(src, dst)
@@ -138,7 +139,7 @@ def main(opt):
         "--USE_PARALLEL", "True",\
         "--NUM_PARALLEL_CORES", "4"\
     ])
-    
+
 
 if __name__ == "__main__":
     opt = parse_opt()
